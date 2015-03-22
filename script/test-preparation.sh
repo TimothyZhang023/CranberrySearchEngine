2c96f19c66b00e5b9328a059302ec9423186598c
zts1993 <zts1993@gmail.com>
2015-03-22 11:26:33 +0800

    finish test script

--- script/test-preparation.sh
+++ script/test-preparation.sh
@@ -9,16 +9,12 @@ cur_dir=$(pwd)
 
 echo cur_dir
 
-rm -rf ../src/main/resources/configure.properties
-rm -rf ../src/test/resources/configure.properties
-ls
-ls ../
-ls ../test
-ls ../test/resources
+rm -rf ./src/main/resources/configure.properties
+rm -rf ./src/test/resources/configure.properties
 
-cp ../src/test/resources/travis-ci.configure.properties ../src/main/resources/
-mv ../src/main/resources/travis-ci.configure.properties ../src/main/resources/configure.properties
-mv ../src/test/resources/travis-ci.configure.properties ../src/test/resources/configure.properties
+cp ./src/test/resources/travis-ci.configure.properties ./src/main/resources/
+mv ./src/main/resources/travis-ci.configure.properties ./src/main/resources/configure.properties
+mv ./src/test/resources/travis-ci.configure.properties ./src/test/resources/configure.properties
 
 echo "========================================================================="
 echo " Test Preparation End"
