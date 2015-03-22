58271895e48d6881f97ba592e5c2b18a029432e3
zts1993 <zts1993@gmail.com>
2015-03-22 11:24:36 +0800

    test

--- script/test-preparation.sh
+++ script/test-preparation.sh
@@ -6,8 +6,16 @@ echo " Test Preparation Start"
 echo "========================================================================="
 
 cur_dir=$(pwd)
+
+echo cur_dir
+
 rm -rf ../src/main/resources/configure.properties
 rm -rf ../src/test/resources/configure.properties
+ls
+ls ../
+ls ../test
+ls ../test/resources
+
 cp ../src/test/resources/travis-ci.configure.properties ../src/main/resources/
 mv ../src/main/resources/travis-ci.configure.properties ../src/main/resources/configure.properties
 mv ../src/test/resources/travis-ci.configure.properties ../src/test/resources/configure.properties
